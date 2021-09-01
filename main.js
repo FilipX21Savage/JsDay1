cd30adb19aaee9f111bbff15d7a1f1d64faca3cf
FilipX21Savage <55578331+FilipX21Savage@users.noreply.github.com>
2021-09-01 10:21:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+/*Exercise 1*/
+
+let baseline = 7; 
+let height = 4; 
+let enHalv = 0.5; 
+
+
+/*Exercise 2*/ 
+
+let firstName = Filip;
+let lastName = Stojanovic; 
+let salary = 10000; 
+let  
+
